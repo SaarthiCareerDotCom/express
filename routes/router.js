726f5bb0ac85b21df61189d5bfceec04be49df0e
AJAY VERMA <verma.av.ajay@gmail.com>
2017-06-09 00:38:00 +0530

    user constructor created

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var MOJO = require('../payment/paymentRequest');
+var User = require('./users');
+var aUser = new User('abcd','xyz@gmail.com');
 /* GET home page. */
-console.log(MOJO);
+console.log(aUser);
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
